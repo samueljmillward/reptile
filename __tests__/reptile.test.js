976e8acec78d5f685b97bc943a3a1eb56f95a6ba
samueljmillward <samuel.j.pearce@hotmail.com>
2019-11-15 23:58:02 +0000

    healthAssess function. notAlive broken

--- __tests__/reptile.test.js
+++ __tests__/reptile.test.js
@@ -4,47 +4,134 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Reptile('Iguana')).toBeInstanceOf(Object);
     });
+
     it('set species property', () => {
         const reptile = new Reptile('Iguana');
-        expect(reptile.species).toEqual('Iguana')
+        expect(reptile.species).toEqual('Iguana');
     });
+
     it('set initial age to 0', () => {
         const reptile = new Reptile('Iguana');
         expect(reptile.age).toEqual(0);
     });
+
     it('set initial fitness to 10', () => {
         const reptile = new Reptile('Iguana');
         expect(reptile.fitness).toEqual(10);
     })
 });
+
 describe('aging function', () => {
     it('increments age by 1', () => {
         const reptile = new Reptile('Iguana');
         reptile.growUp();
         expect(reptile.age).toEqual(1);
     });
+
     it('decrement fitness by 3', () => {
         const reptile = new Reptile('Iguana');
         reptile.growUp();
         expect(reptile.fitness).toEqual(7);
     });
+
     it('increments hunger by 5', () => {
         const reptile = new Reptile('Iguana');
         reptile.growUp();
         expect(reptile.hunger).toEqual(5);
     });
+    it('throws an error if the reptile is not alive', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.hunger = 10;
+        expect(() => reptile.feed()).toThrow('Catastrophic organ failure');
+    });
 });
-describe('walking function', () => {
+describe('hunting function', () => {
     it('increments fitness by 4', () => {
         const reptile = new Reptile('Iguana');
         reptile.fitness = 4
-        reptile.walk();
+        reptile.hunt();
         expect(reptile.fitness).toEqual(8);
     });
     it('fitness cannot increase over 10', () => {
         const reptile = new Reptile('Iguana');
         reptile.growUp();
-        reptile.walk();
+        reptile.hunt();
         expect(reptile.fitness).toEqual(10);
+    });
+    it('throws an error if the reptile is not alive', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.fitness = 0;
+        expect(() => reptile.feed()).toThrow('Catastrophic organ failure');
+    });
+});
+
+describe('feeding function', () => {
+    it('decrement hunger by 3', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.growUp();
+        reptile.feed();
+        expect(reptile.hunger).toEqual(2);
+    });
+
+    it('hunger cannot decrease below 0', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.growUp();
+        reptile.feed();
+        reptile.feed();
+        expect(reptile.hunger).toEqual(0);
+    });
+
+    it('throws an error if the reptile is not alive', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.age = 30;
+        expect(() => reptile.feed()).toThrow('Catastrophic organ failure');
+    });
+});
+describe('basking function', () => {
+    it('returns reptile body heat', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.bask();
+        expect(reptile.bodyHeat).toEqual(70);
+    });
+
+    it('body heat cannot decrease below 0', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.hunt()
+        reptile.hunt()
+        reptile.hunt()
+        expect(reptile.bodyHeat).toEqual(0);
+    });
+});
+describe('assessHealth function', () => {
+    it('returns reptile status', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.hunger = 4
+        reptile.fitness = 4
+        reptile.assessHealth();
+        expect(reptile.assessHealth()).toBe("Condition optimal. Recommend returning to safety")
+    });
+
+    it('returns reptile status', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.hunger = 8;
+        reptile.fitness = 1;
+        reptile.assessHealth();
+        expect(reptile.assessHealth()).toBe("Condition critical")
+    });
+
+    it('returns reptile status', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.hunger = 6
+        reptile.fitness = 4
+        reptile.assessHealth();
+        expect(reptile.assessHealth()).toBe("I must feed")
+    });
+
+    it('returns reptile status', () => {
+        const reptile = new Reptile('Iguana');
+        reptile.hunger = 4
+        reptile.fitness = 1
+        reptile.assessHealth();
+        expect(reptile.assessHealth()).toBe("I must hunt")
     });
 });
--- src/reptile.js
+++ src/reptile.js
@@ -1,10 +1,18 @@
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+const MINIMUM_BODYHEAT = 0;
+
 function Reptile(species) {
     this.species = species
     this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
-
+    this.hunger = MINIMUM_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
+    this.bodyHeat = 40;
 };
+
+Reptile.prototype.isAlive = function () {
+    return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+}
 
 Reptile.prototype.growUp = function () {
     this.age += 1;
@@ -12,11 +20,45 @@ Reptile.prototype.growUp = function () {
     this.hunger += 5;
 };
 
-Reptile.prototype.walk = function () {
-    if ((this.fitness + 4) <= 10) {
+Reptile.prototype.hunt = function () {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
         this.fitness += 4;
     } else {
-        this.fitness = 10;
+        this.fitness = MAXIMUM_FITNESS;
+    };
+    this.bodyHeat -= 20;
+};
+
+Reptile.prototype.feed = function () {
+    if (!this.isAlive) {
+        throw new Error('Catastrophic organ failure');
+    }
+    this.hunger -= 3;
+    if (this.hunger < MINIMUM_HUNGER) {
+        this.hunger = MINIMUM_HUNGER
+    };
+    this.bodyHeat -= 10;
+};
+
+Reptile.prototype.bask = function () {
+    this.bodyHeat += 30;
+    if (this.bodyHeat < 0) {
+        this.bodyHeat = 0
     };
 };
+
+Reptile.prototype.assessHealth = function () {
+    if (this.fitness <= 3 && this.hunger >= 5) {
+        return "Condition critical";
+    };
+    if (this.fitness <= 3) {
+        return "I must hunt";
+    };
+    if (this.hunger >= 5) {
+        return "I must feed";
+    } else {
+        return "Condition optimal. Recommend returning to safety";
+    };
+};
+
 module.exports = Reptile;
